5a5a1194f89289d7368ff847a0de78acfc69a9f0
Alper Yildirim <alperyldrm96@gmail.com>
2024-01-14 15:34:29 +0300

    cleanBlog_Bolum2

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const express = require('express');
-
+const path = require('path');
+const ejs = require('ejs');
 const app = express();
 
+//Template Engine
+app.set('view engine', 'ejs');
+
+//MIDDLEWARE
+app.use(express.static('public'));
+
+//ROUTES
 app.get('/', (req, res) => {
-  const blog = {
-    ID: '1',
-    title: 'Blog Title',
-    description: 'Blog Description',
-  };
-  res.send(blog);
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
 });
 
+//PORT
 const port = 5000;
 
 app.listen(port, () => {
